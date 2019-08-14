5c0ccd82180955518dc0ec118eb01b3adebf87ae
errorCreate <nm784546819@163.com>
2019-08-14 13:44:05 +0800

    初始化示例项目——配置

---
+++ demo-properties/src/main/java/com/errorcreate/demo/properties/DemoPropertiesApplication.java
@@ -0,0 +1,18 @@
+package com.errorcreate.demo.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author errorCreate
+ */
+@SpringBootApplication
+public class DemoPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPropertiesApplication.class, args);
+    }
+
+}
