662d5460e49435b7258bc1a549e1e2360cf160a8
errorCreate <nm784546819@163.com>
2019-08-17 18:19:09 +0800

    配置多环境示例

---
+++ demo-environment/src/main/java/com/errorcreate/demo/environment/DemoEnvironmentApplication.java
@@ -0,0 +1,18 @@
+package com.errorcreate.demo.environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author errorCreate
+ */
+@SpringBootApplication
+public class DemoEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEnvironmentApplication.class, args);
+    }
+
+}
---
+++ demo-environment/src/test/java/com/errorcreate/demo/environment/DemoEnvironmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.errorcreate.demo.environment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoEnvironmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
