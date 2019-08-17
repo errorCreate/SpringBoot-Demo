2fcd0bd0c5d69c573d1e116bc690884420fa4268
errorCreate <nm784546819@163.com>
2019-08-17 16:42:32 +0800

    配置profiles示例

---
+++ demo-profiles/src/main/java/com/errorcreate/demo/profiles/DemoProfilesApplication.java
@@ -0,0 +1,18 @@
+package com.errorcreate.demo.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author errorCreate
+ */
+@SpringBootApplication
+public class DemoProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProfilesApplication.class, args);
+    }
+
+}
